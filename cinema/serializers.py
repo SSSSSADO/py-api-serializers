951e5868d70ced11f1920c18e955b85bfde2bdf1
SSSSSADO <grushkovik.danil@gmail.com>
2026-04-15 21:11:41 +0300

    fix ЬщÐMovieListSerializer & settings.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -53,7 +53,14 @@ class MovieListSerializer(MovieSerializer):
         read_only=True,
         slug_field="name"
     )
-    actors = serializers.StringRelatedField(many=True, read_only=True)
+
+    actors = serializers.SerializerMethodField()
+
+    def get_actors(self, obj: Movie) -> list[str]:
+        return [
+            f"{actor.first_name} {actor.last_name}"
+            for actor in obj.actors.all()
+        ]
 
 
 class MovieRetrieveSerializer(MovieSerializer):
